054c45314430275a237a93093389667eb76b74a3
sathish Jarugumalli <jsathishkumarc@gmail.com>
2024-04-29 23:41:11 +0530

    Update vertical_sum.c

--- vertical_sum.c
+++ vertical_sum.c
@@ -145,3 +145,38 @@ int main()
     printf("\n");
     return 0;
 }
+
+
+second Method:
+==============
+// Traverses the tree in in-order form and
+// populates a hashMap that contains the
+// vertical sum
+void verticalSumUtil(Node *node, int hd,
+                     map<int, int> &Map)
+{
+    // Base case
+    if (node == NULL) return;
+ 
+    // Recur for left subtree
+    verticalSumUtil(node->left, hd-1, Map);
+ 
+    // Add val of current node to
+    // map entry of corresponding hd
+    Map[hd] += node->data;
+ 
+    // Recur for right subtree
+    verticalSumUtil(node->right, hd+1, Map);
+}
+ 
+// Function to find vertical sum
+void verticalSum(Node *root)
+{
+    // a map to store sum of nodes for each 
+    // horizontal distance
+    map < int, int> Map;
+    map < int, int> :: iterator it;
+ 
+    // populate the map
+    verticalSumUtil(root, 0, Map);
+ 
