275df3dc73355bf3547644775f0a54106bec199f
sathish Jarugumalli <jsathishkumarc@gmail.com>
2024-04-29 23:16:18 +0530

    Update level_order_traversal.c

--- level_order_traversal.c
+++ level_order_traversal.c
@@ -85,3 +85,60 @@ int main()
 
 	return 0;
 }
+
+
+
+
+void printLevelOrder(Node* root)
+{
+    // Base Case
+    if (root == NULL)
+        return;
+
+    // Create an empty queue for level order traversal
+    queue<Node*> q;
+
+    // Enqueue Root and initialize height
+    q.push(root);
+
+    while (q.empty() == false) {
+
+        // Print front of queue and remove it from queue
+        Node* node = q.front();
+        cout << node->data << " ";
+        q.pop();
+
+        // Enqueue left child
+        if (node->left != NULL)
+            q.push(node->left);
+
+        // Enqueue right child
+        if (node->right != NULL)
+            q.push(node->right);
+    }
+}
+
+
+// Given a binary tree, print its nodes in level order
+// using array for implementing queue
+void printLevelOrder(struct node* root)
+{
+    int rear, front;
+    struct node** queue = createQueue(&front, &rear);
+    struct node* temp_node = root;
+
+    while (temp_node) {
+        printf("%d ", temp_node->data);
+
+        // Enqueue left child
+        if (temp_node->left)
+            enQueue(queue, &rear, temp_node->left);
+
+        // Enqueue right child
+        if (temp_node->right)
+            enQueue(queue, &rear, temp_node->right);
+
+        // Dequeue node and make it temp_node
+        temp_node = deQueue(queue, &front);
+    }
+}
